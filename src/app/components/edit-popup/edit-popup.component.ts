76e9fe8b6c63b2c322b2e4f4298c96bdd5b6bae2
Dorin Dura <duradorin11@yahoo.com>
2024-03-21 10:13:58 +0200

    AngularApp

---
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { Product } from '../../../types';
+import { FormBuilder, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { RatingModule } from 'primeng/rating'
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-edit-popup',
+  standalone: true,
+  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule, ReactiveFormsModule],
+  templateUrl: './edit-popup.component.html',
+  styleUrl: './edit-popup.component.scss'
+})
+export class EditPopupComponent {
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  @Input() display: boolean = false;
+  @Output() displayChange = new EventEmitter<boolean>();
+
+  // @Output() cancel = new EventEmitter<void>();
+
+  @Input() header!: string;
+
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  @Output() confirm = new EventEmitter<Product>();
+
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};\'|"\\|,.\/?]+/.test(
+        control.value
+      )
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true} : null;
+    };
+  }
+
+  productForm = this.formBuilder.group({
+    name: ['', [Validators.required, this.specialCharacterValidator()]],
+    image: [''],
+    price: ['', [Validators.required]],
+    rating: [0],
+  })
+
+  ngOnChanges() {
+    this.productForm.patchValue({
+      name: this.product.name,
+      image: this.product.image,
+      price: this.product.price,
+      rating: this.product.rating,
+    });
+  }
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+    this.display = false;
+    this.displayChange.emit(this.display);
+  }
+
+  onCancel() {
+    this.display = false;
+    this.displayChange.emit(this.display);
+  }
+}
